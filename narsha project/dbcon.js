017602651e59dc2142c8ffedc9c59d4e5de7ed48
godbs129 <junhaeyoon@gmail.com>
2020-11-03 17:10:13 +0900

    dbcon update

--- narsha project/dbcon.js
+++ narsha project/dbcon.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const con = mysql.createPool({
-    host:'localhost',
-    database:'shareboard',
-    user:'root',
-    password:'pure6671',
-    connectionLimit:30
+    host: '10.80.163.222',
+    database: 'narsha',
+    user: 'root',
+    password: 'jhy040129',
+    connectionLimit: 30
 });
 
 module.exports = con;
