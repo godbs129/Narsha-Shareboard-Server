8da33b59e25b5171aa8e4ce43d4121a8f8bf1cf0
joo01029 <joo01029@naver.com>
2020-11-10 09:25:05 +0900

    clipboard select update

--- Narsha-project/router/clipboard-select.js
+++ Narsha-project/router/clipboard-select.js
@@ -34,7 +34,7 @@ router.get('/clipboard/:board', (req, res) => {
         const selectboard = (userId) => {
             const p = new Promise((resolve, reject) => {
                 connection.query(`select c.boardId, c.board, c.deviceId, c.date, d.deviceName, d.userId, d.typeId ` +
-                    `from clipboard as c join device as d on c.deviceId = d.deviceId where d.userId = ? and c.board like = ?`, [userId, '%' + board + '%'], (err, result) => {
+                    `from clipboard as c join device as d on c.deviceId = d.deviceId where d.userId = ? and c.board like = ?`, [userId, "'%" + board + '%;'], (err, result) => {
                         if (err) {
                             console.log(err.message);
                             reject(err);
